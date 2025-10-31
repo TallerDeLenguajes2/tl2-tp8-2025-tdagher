807ad53da329b32d01f256326892f4254d1cbb99
tdagher <tomasdaghero99@gmail.com>
2025-10-31 09:46:20 -0300

    proyecto mvc creado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tl2_tp8_2025_tdagher.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
